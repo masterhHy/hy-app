d03b451802ea7704996fd0f10c92ae8b8a6d8d16
guo <532246081@qq.com>
2018-12-07 16:31:50 +0800

    修改配置

--- time-coin2/src/plugins__config/auth-client/utils/config.js
+++ time-coin2/src/plugins__config/auth-client/utils/config.js
@@ -3,6 +3,6 @@ export default {
   appId: 'admin',
   appSecret: '123456',
   authUrl: 'http://localhost:18001/auth',
-  baseUrl: 'http://localhost:8080/',
-  resourceUrl:"http://localhost:18000/client-time-coin/api/"
+  resourceUrl:"http://localhost:18000/client-time-coin/api/",
+  baseUrl: 'http://localhost:8080/'
 }
