debd8da8d17e6e7c91acd238132cd09e3d7545db
guo <532246081@qq.com>
2019-01-08 13:48:06 +0800

    依赖跟新 打包编译bug

--- time-coin2/build/webpack.base.conf.js
+++ time-coin2/build/webpack.base.conf.js
@@ -38,7 +38,7 @@ let webpackConfig = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'),resolve('/node_modules/vue-calendar-component/lib')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
