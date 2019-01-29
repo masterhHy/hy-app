c5a7a30afc55724e8f933d00bbe0f5a0fc080443
Hy <532246081@qq.com>
2019-01-29 12:36:23 +0800

    配置文件

--- admin/src/plugins/auth-client/utils/config.js
+++ admin/src/plugins/auth-client/utils/config.js
@@ -2,9 +2,14 @@
 export default {
   appId: 'admin',
   appSecret: '123456',
-  //authUrl: 'http://localhost:18001/auth',
-  //resourceUrl:"http://localhost:18000/user-service-provider/api/",
+  authUrl: 'http://localhost:18001/auth',
+  resourceUrl:"http://localhost:18000/user-service-provider/api/",
+  baseUrl: 'http://localhost:8080/'
+}
+/*export default {
+  appId: 'admin-dev',
+  appSecret: '123456',
   authUrl: 'http://www.ttimecoin.top/auth',
   resourceUrl:"http://www.ttimecoin.top/user-service-provider/api/",
   baseUrl: 'http://localhost:8080/'
-}
+}*/
