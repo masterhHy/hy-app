e50c734960f068124ad74588062d24876e18f2c1
wuliya <wuliya@iplas.com.cn>
2019-09-09 18:43:05 +0800

    修改树洞

---
+++ time-coin2/src/components/h5/wishlist/EditWish.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="add-wish">
+    <div class="add-head">
+      <span class="logo"><img class="coins-img" src="@/assets/images/wish1.png" style="height: 40px;"></span>
+      <span class="icon mg-r8" @click="save"><i class="fas fa-check"></i></span>
+      <span class="icon mg-r6" @click="showDialog"><i class="fas fa-times"></i></span>
+      <div v-transfer-dom>
+        <confirm  v-model="show" title="温馨提示" @on-cancel="onCancel"  @on-confirm="onConfirm">
+          <p style="text-align:center;">确定要退出么，退出将无法保存哟？</p>
+        </confirm>
+      </div>
+    </div>
+    <div class="add-content">
+      <h4>标题</h4>
+      <input type="text" v-model="content.title" placeholder="<未命名>">
+      <h4>价格</h4>
+      <input type="number" v-model="content.price" placeholder="0.00">
+      <h4>优先级</h4>
+      <input type="number" v-model="content.priority">
+      <h4>具体描述</h4>
+      <div class="details">
+        <p>{{content.description}}</p>
+        <textarea v-model="content.description" placeholder="请描述一下您的具体愿望"
+                  style="min-height: 80px;width: 95%;border: none; "></textarea>
+      </div>
+      <h4 class="last-text"></h4>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {Confirm, TransferDomDirective as TransferDom} from 'vux'
+
+  export default {
+    name: 'EditWish',
+    directives: {
+      TransferDom
+    },
+    components:{Confirm},
+    data() {
+      return {
+        content: {
+          title: '',
+          price: null,
+          priority: null,
+          description: ''
+        },
+        show:false
+      }
+    },
+    methods: {
+      onCancel () {
+      },
+      onConfirm () {
+       this.$router.push({name:'wishList'})
+      },
+      showDialog () {
+        this.show = true
+      },
+      //保存新建愿望
+      save () {
+        let param = {}
+        Object.assign(param, this.content)
+        this.axios({
+          url:'/dream/addOrUpdate',
+          method:'get',
+          params:param
+        }).then((res)=>{
+          this.$vux.loading.hide()
+          this.$router.push({name:'wishList'})
+        }).catch(()=>{})
+      },
+      mounted () {
+        if (this.$route.params) {
+          this.id = this.$route.params.id
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .add-content {
+    width: 100%;
+  }
+
+  .details {
+    position: relative;
+  }
+
+  .add-content h4, .add-content textarea, .details p {
+    font-size: 14px;
+    border-bottom: 1px solid lightgray;
+    margin: 10px;
+    letter-spacing: 1px;
+  }
+
+  .details textarea {
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .details p {
+    display: block;
+    visibility: hidden;
+    min-height: 80px;
+    width: 100%;
+  }
+
+  .add-content input {
+    border: none;
+    margin: 10px;
+  }
+
+  .add-head {
+    width: 100%;
+    height: 40px;
+    background-color: #363636;
+  }
+
+  .icon {
+    font-size: 20px;
+    color: lightgray;
+    float: right;
+    display: inline-block;
+    height: 40px;
+    line-height: 40px;
+  }
+
+  .mg-r8 {
+    margin-right: 8%;
+  }
+
+  .mg-r6 {
+    margin-right: 6%;
+  }
+
+  .logo {
+    margin-left: 5%;
+  }
+
+  .add-content .last-text {
+    border: 1px solid lightgray;
+    margin-top: 20px;
+    height: 8px;
+    border-top: none;
+
+  }
+</style>
---
+++ tree-hole/components/allHole/testCreateApi.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <cube-button @click="showCascadePicker">Cascade Picker</cube-button>
+  </div>
+</template>
+
+<script>
+  const cascadeData = [
+    {
+      value: 'Fruit',
+      text: 'Fruit',
+      children: [
+        {
+          value: 'Apple',
+          text: 'Apple',
+          children: [{ value: 1, text: 'One' }, { value: 2, text: 'Two' }]
+        },
+        {
+          value: 'Orange',
+          text: 'Orange',
+          children: [{ value: 3, text: 'Three' }, { value: 4, text: 'Four' }]
+        }
+      ]
+    },
+    {
+      value: 'Drink',
+      text: 'Drink',
+      children: [
+        {
+          value: 'Coffee',
+          text: 'Coffee',
+          children: [{ value: 1, text: 'One' }, { value: 2, text: 'Two' }]
+        },
+        {
+          value: 'Tea',
+          text: 'Tea',
+          children: [{ value: 1, text: 'One' }, { value: 3, text: 'Three' }]
+        }
+      ]
+    }
+  ]
+export default {
+    name: 'testCreateApi',
+    mounted () {
+      this.cascadePicker = this.$createCascadePicker({
+        title: 'Cascade Picker',
+        data: cascadeData,
+        selectedIndex: [0, 1, 0],
+        onSelect: this.selectHandle,
+        onCancel: this.cancelHandle
+      })
+    },
+    methods: {
+      showCascadePicker () {
+        this.cascadePicker.show()
+      },
+      selectHandle (selectedVal, selectedIndex, selectedText) {
+        this.$createDialog({
+          type: 'warn',
+          content: `Selected Item: <br/> - value: ${selectedVal.join(', ')} <br/> - index: ${selectedIndex.join(', ')} <br/> - text: ${selectedText.join(' ')}`,
+          icon: 'cubeic-alert'
+        }).show()
+      },
+      cancelHandle () {
+        this.$createToast({
+          type: 'correct',
+          txt: 'Picker canceled',
+          time: 1000
+        }).show()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
